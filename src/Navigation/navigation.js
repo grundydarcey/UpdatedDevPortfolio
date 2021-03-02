984790fc71ad61566e23387f1f060775eca80687
Beyonce Knowles <you@example.com>
2021-03-01 22:07:46 -0500

    fixed footer and nav

--- src/Navigation/navigation.js
+++ src/Navigation/navigation.js
@@ -14,8 +14,8 @@ export default function Navigation() {
         </div>
         <div className='righthalf'>
           <div className='alllinks'>
-          <Link to='/home' className='link'>HOME</Link>&nbsp; &nbsp; &nbsp;
-          <Link to='/contact' className='link'>CONTACT</Link>&nbsp; &nbsp; &nbsp;
+          <Link to='/home' className='link'>HOME</Link>
+          <Link to='/contact' className='link'>CONTACT</Link>
           <Link to='/portfolio'className='link'>PROJECTS</Link>
         </div>
         </div>
