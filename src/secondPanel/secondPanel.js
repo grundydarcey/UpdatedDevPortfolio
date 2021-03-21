90bddf9f8fd9e173fe5ee88d3e993f6e1724b929
Beyonce Knowles <you@example.com>
2021-03-21 18:20:31 -0400

    test

--- src/secondPanel/secondPanel.js
+++ src/secondPanel/secondPanel.js
@@ -8,7 +8,7 @@ export default function secondPanel() {
       <section className='container'>
         <section className='text'>
           <h1>SKILLS</h1><br />
-          <p>She was able to learn Javascript, React.js, Express, PostgreSQL, Node.js, data structures, algorithms, jQuery, AJAX, HTML5, and CSS3 and worked with all of them in different combination per assigned project. These new skills built upon a foundation already fostered by Myspace and an urge to create.</p>
+          <p>She was able to learn Javascript, React.JS, Express, PostgreSQL, Node.js, data structures, algorithms, jQuery, AJAX, HTML5, and CSS3 and worked with all of them in different combination per assigned project. These new skills built upon a foundation already fostered by Myspace and an urge to create.</p>
         </section>
         <section className='image'>
           <img src={Textbook} className='textbook' alt='textbook' />
