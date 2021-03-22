1c33e4e7e5f3f73884941d08efec86f0e8009b56
Beyonce Knowles <you@example.com>
2021-03-21 22:08:42 -0400

    revision

--- src/Home/home.js
+++ src/Home/home.js
@@ -8,8 +8,9 @@ export default function Home() {
   return (
     <section className='home'>
       <section className='textbox'>
-        <h1>About Darcey</h1><br />
-        <p>Darcey is an aspiring software engineer looking to find a company match that suits her recent experience in an immersive engineering course. She has past experience in the spa and wellness industry but due to a recent furlough, she began working on a home-based business of crafting with a website accompanying it to increase sales. This exposed her to the absolute necessity of a web presence for anyone trying to get anything done in the world. She began dabbling in software engineering and venturing outside of pre-written templates, and into formulating her own ideas. Soon after, she enrolled in Thinkful's immersive Software Engineering program. With the course as a guide, she was able to complete multiple full-stack projects showing mastery of all concepts learned within it.</p><br /><br />
+        <h1>About Me</h1><br />
+        <p>My name is Darcey Grundy and I'm an aspiring software engineer. I'm looking to find a company match that suits my recent experience in an immersive engineering course. I'm excited to learn from senior developers and junior developers alike as I gain experience in the industry. I have past experience in the spa and wellness industry but after being furloughed I decided to pivot. I began working on a home-based business of crafting with a website accompanying it to increase sales. This exposed me to the absolute necessity of a web presence for anyone trying to get anything done. I began dabbling in software engineering and ventured outside of pre-written templates, and into formulating my ideas. Soon after, I enrolled in Thinkful's immersive Software Engineering program. This has been a very exciting time of seeing all the fun and vital projects I can help bring into the world. With the course as a guide, I was able to complete multiple full-stack projects showing mastery of all concepts learned within it.</p>
+        <p>Outside of work and school, I enjoy cooking and spending time in nature. Living in Florida gives me many opportunities to explore our two beautiful seasons, summer and not-summer.</p><br /><br />
       </section>
       <section className='panels'>
         <FirstPanel />
