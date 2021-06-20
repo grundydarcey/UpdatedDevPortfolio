04818cb143659dd2850769a37740f2c701749f4c
Beyonce Knowles <you@example.com>
2021-06-20 13:25:29 -0400

    added project and changed social links to fontawesome

--- src/Links/links.js
+++ src/Links/links.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './link.css';
-import GitHub from '../Images/github.webp';
-import LinkedIn from '../Images/imglinkedin.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Links() {
   return (
     <section className='links'>
       <section className='gitHubLink'>
         <a href='https://github.com/grundydarcey/' target='_blank' rel='noreferrer'>
-          <img src={GitHub} alt='GitHub' className='gitHub' />
+          <FontAwesomeIcon 
+            icon={faGithub}
+            color='#000'
+          />
         </a>
         </section>
         <section className='linkedInLink'>
           <a href='https://www.linkedin.com/in/darcey-grundy/' target='_blank' rel='noreferrer'>
-            <img src={LinkedIn} alt='LinkedIn' className='linkedIn' />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color='#000'
+            />
           </a>
       </section>
     </section>
